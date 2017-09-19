7f97536f0ab7870f571be0242da028fb08f4e145
Eldin Kajdic <eldinkajdic@hotmail.com>
2017-09-19 13:04:46 +0200

    Changed code for mediaplayers and added extended commentary

--- app/src/main/java/com/example/pimpjuice/motionsensor/MainActivity.java
+++ app/src/main/java/com/example/pimpjuice/motionsensor/MainActivity.java
@@ -19,46 +19,55 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+
     private TextView xText, yText, zText;
     private Sensor motionSensor;
     private SensorManager sensorManager;
     private ColorStateList originalColor;
     private ToggleButton toggleButton;
+
+    // Used to start and stop the program
     private boolean on;
-    MediaPlayer lazerSound;
-    MediaPlayer beatBox;
-    MediaPlayer gunshot;
+
+    // The different soundplayers used in the program
+    MediaPlayer lazerSound, beatBox, gunshot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Här skapas sensormanagern
+        // Creates the SensorManager used in the program
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
 
-        // Här skapas sensorn
+        // Sets the Sensor motionSensor to type Accelerometer
         motionSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        // Skapar sensor lyssnaren
+        // Sets the speed of the SensorManager to FASTEST
         sensorManager.registerListener(this, motionSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
-        // Hämtar värden
+        // Collects the values from the mainpage
         xText = (TextView)findViewById(R.id.bokstavX);
         zText = (TextView)findViewById(R.id.bokstavZ);
         yText = (TextView)findViewById(R.id.bokstavY);
 
+        // Used to reset the texts back to default after coloring further down
         originalColor =  xText.getTextColors();
+
+        // Togglebutton used to start and stop the program
         toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
 
+        // Imports the different soundfiles from the raw directory and assigns them
         lazerSound = MediaPlayer.create(this, R.raw.lasershot);
         beatBox = MediaPlayer.create(this, R.raw.beatbox);
         gunshot = MediaPlayer.create(this, R.raw.gunshot);
+
+        // Sets the bool on to false/not clicked as a default
         on = false;
     }
 
     public void onToggleClicked(View view) {
-            // Is the toggle on?
+
             if(toggleButton.isChecked()) {
                 on = true;
             }
@@ -70,48 +79,47 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
 
+        // If the app is in landscape mode, automaticly start the program (cheap fix of a bug)
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
             on = true;
             beatBox.reset();
             gunshot.reset();
-
         }
 
+        // If the toggle button has been clicked
         if (on == true) {
 
+            // Set the text to the current values of the accelerometer
         xText.setText("X: " + event.values[0]);
         zText.setText("Z: " + event.values[1]);
         yText.setText("Y: " + event.values[2]);
 
+            // If phone is leaned towards the X value
             if (event.values[0] > event.values[1] && event.values[0] > event.values[2]) {
-
                 lazerSound.start();
                 xText.setTextColor(Color.RED);
                 zText.setTextColor(originalColor);
                 yText.setTextColor(originalColor);
-
-
-            } else if (event.values[1] > event.values[0] && event.values[1] > event.values[2]) {
-
+            }
+            // If phone is leaned towards the Z value
+            else if (event.values[1] > event.values[0] && event.values[1] > event.values[2]) {
                 beatBox.start();
                 xText.setTextColor(originalColor);
                 zText.setTextColor(Color.GREEN);
                 yText.setTextColor(originalColor);
-
-
-            } else if (event.values[2] > event.values[0] && event.values[2] > event.values[1]) {
-
+            }
+            // If phone is leaned towards the Y value
+            else if (event.values[2] > event.values[0] && event.values[2] > event.values[1]) {
                 gunshot.start();
                 xText.setTextColor(originalColor);
                 zText.setTextColor(originalColor);
                 yText.setTextColor(Color.BLUE);
-
             }
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    // Används inte
+    // Is needed but not in use
     }
 }
